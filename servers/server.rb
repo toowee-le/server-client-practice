77de415622323dc0b818c6c5e2e2a932b416c95f
Tuyet Le <toowee.le@gmail.com>
2021-01-05 14:50:25 +0000

    Update the server to continue running after user types 'quit'

--- servers/server.rb
+++ servers/server.rb
@@ -1,19 +1,30 @@
 require 'socket'
 
-server = TCPServer.new(2345)
+def run_server
+  server = TCPServer.new(2345)
 
-socket = server.accept
-
-while true
-  socket.puts "What do you say?"
-  they_said = socket.gets.chomp
-
-  if they_said != "quit"
-    socket.puts "You said: #{they_said}."
-  else
-    socket.puts "You said: #{they_said}. Goodbye!"
-    break
+  loop do
+    socket = server.accept
+  
+    get_response(socket)
   end
+
+  socket.close
 end
 
-socket.close
+def get_response(socket)
+  loop do
+    socket.puts "What do you say?"
+    they_said = socket.gets.chomp
+
+    if they_said != "quit"
+      socket.puts "You said: #{they_said}."
+    else
+      socket.puts "You said: #{they_said}. Goodbye!"
+      break
+    end
+  end
+  false
+end
+
+run_server
