539d94493d61acdf0e216a6d4d009e82af0f1e86
Tuyet Le <toowee.le@gmail.com>
2021-01-05 18:09:13 +0000

    Add comments

--- clients/client.rb
+++ clients/client.rb
@@ -2,7 +2,7 @@ require 'socket'
 
 socket = TCPSocket.new('localhost', 2345)
 
-puts socket.gets
+puts socket.gets # Read lines from socket
 puts "Wow what a rude server... I'm going to say Sarah"
 socket.puts "Sarah"
 puts socket.gets
