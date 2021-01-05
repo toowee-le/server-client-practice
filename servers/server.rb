06277262da98cbe5d247e04313b47bc21df0e988
Tuyet Le <toowee.le@gmail.com>
2021-01-05 12:16:37 +0000

    Make user stay connected to server until they type 'quit'

--- servers/server.rb
+++ servers/server.rb
@@ -4,10 +4,16 @@ server = TCPServer.new(2345)
 
 socket = server.accept
 
-socket.puts "What do you say?"
+while true
+  socket.puts "What do you say?"
+  they_said = socket.gets.chomp
 
-they_said = socket.gets.chomp
-
-socket.puts "You said: #{they_said}. Goodbye!"
+  if they_said != "quit"
+    socket.puts "You said: #{they_said}."
+  else
+    socket.puts "You said: #{they_said}. Goodbye!"
+    break
+  end
+end
 
 socket.close
